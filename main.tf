033746d4b3171929d89ad259a0d6b237e1387813
Lorris Hamdaoui <lorris.hamdaoui@epitech.eu>
2020-12-17 13:23:28 +0100

    better main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
+terraform {
+  required_providers {
+    boundary = {
+      source  = "hashicorp/boundary"
+      version = "0.1.0"
+    }
+  }
+}
+
 provider "boundary" {
-  addr                            = "http://127.0.0.1:9200"
-  auth_method_id                  = "ampw_1234567890"
-  password_auth_method_login_name = "admin"
-  password_auth_method_password   = "password"
+  addr             = "http://127.0.0.1:9200"
+#   recovery_kms_hcl = <<EOT
+# kms "aead" {
+#   purpose = "recovery"
+#   aead_type = "aes-gcm"
+#   key = "lvPKw0QaWaaSR5t/AblmfhDkD1F/J+MYH8uKZrgAeds="
+#   key_id = "global_recovery"
+# }
+# EOT
 }
