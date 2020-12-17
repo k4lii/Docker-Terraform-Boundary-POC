13f5fc3a8b6d561d9381b2c09306e1e5f0787e84
Lorris Hamdaoui <lorris.hamdaoui@epitech.eu>
2020-12-17 14:24:26 +0100

    wekly objective OK, boundary server is deployed into a docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,12 @@ terraform {
 
 provider "boundary" {
   addr             = "http://127.0.0.1:9200"
-#   recovery_kms_hcl = <<EOT
-# kms "aead" {
-#   purpose = "recovery"
-#   aead_type = "aes-gcm"
-#   key = "lvPKw0QaWaaSR5t/AblmfhDkD1F/J+MYH8uKZrgAeds="
-#   key_id = "global_recovery"
-# }
-# EOT
+   recovery_kms_hcl = <<EOT
+ kms "aead" {
+   purpose = "recovery"
+   aead_type = "aes-gcm"
+   key = "8fZBjCUfN0TzjEGLQldGY4+iE9AkOvCfjh7+p0GtRBQ="
+   key_id = "global_recovery"
+ }
+ EOT
 }
