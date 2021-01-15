d8aff189e56262e839a11a76c099c1903e64c757
Lorris Hamdaoui <lorris.hamdaoui@epitech.eu>
2021-01-15 15:31:11 +0100

    little update

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,16 +8,16 @@ terraform {
 }
 
 provider "boundary" {
-  addr             = "http://127.0.0.1:9200"
-  auth_method_id                  = "ampw_1234567890"
-    password_auth_method_login_name = "admin"
-  password_auth_method_password   = "password"
-   recovery_kms_hcl  = <<EOT
- kms "aead" {
-  purpose = "recovery"
+  addr                            = "http://127.0.0.1:9200"
+//   auth_method_id                  = "ampw_1234567890"
+//     password_auth_method_login_name = "admin"
+//   password_auth_method_password   = "password"
+  recovery_kms_hcl = <<EOT
+kms "aead" {
+	purpose = "recovery"
   aead_type = "aes-gcm"
   key = "8fZBjCUfN0TzjEGLQldGY4+iE9AkOvCfjh7+p0GtRBQ="
-  key_id = "global_root"
+  key_id = "global_worker-auth"
 }
- EOT
+EOT
 }
